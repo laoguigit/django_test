cbef9a95ade05523521fc02fbf3c727b4994dffa
Feng <Xiang.Feng@Honeywell.com>
2021-07-05 17:38:50 +0800

    get client ip

--- proj1/app1/views.py
+++ proj1/app1/views.py
@@ -3,5 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def f1(request):
+    # x = {'a':1,'b':2}
+    # 'a' not in x.keys()
+
     print('xxxxxx')
+    # if request.META.has_key('HTTP_X_FORWARDED_FOR'):
+    if 'HTTP_X_FORWARDED_FOR' in request.META.keys():
+        ip = request.META['HTTP_X_FORWARDED_FOR']
+    else:
+        ip = request.META['REMOTE_ADDR']
+    print(ip)
     return HttpResponse('<h1>xxxxx</h1>')
