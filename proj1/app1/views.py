676eae5705383067e7c0131d06fff0d9b6919855
Feng <Xiang.Feng@Honeywell.com>
2021-07-05 17:27:34 +0800

    add app1

--- proj1/app1/views.py
+++ proj1/app1/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def f1(request):
+    print('xxxxxx')
+    return HttpResponse('<h1>xxxxx</h1>')
